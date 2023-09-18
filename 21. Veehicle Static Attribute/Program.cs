362d724f400ee57d1d30621a530aa5f506d1a111
Dell-User <Dell-User@DESKTOP-B16SGH2>
2023-09-18 14:29:30 +0200

    Add project files.

---
+++ 21. Veehicle Static Attribute/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21.Veehicle_Static_Attribute
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Vehicle vehicle1 = new Vehicle("V001","Toyota","green");
+            Vehicle vehicle2 = new Vehicle("V002", "Ferari", "Red");
+            Vehicle vehicle3 = new Vehicle("V003", "Mazda", "Black");
+            Console.WriteLine("The total number of vehicles registered is: " + Vehicle.vehicleCount);
+
+
+            //freeze console
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 21. Veehicle Static Attribute/Vehicle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21.Veehicle_Static_Attribute
+{
+    internal class Vehicle
+    {
+        public string id;
+        public string brand;
+        public string color;
+
+        //Static attribute
+        public static int vehicleCount;
+        //call the constructor
+        public Vehicle(string aId,string aBrand,string aColor) {
+            id = aId;
+            brand = aBrand;
+            color = aColor;
+            vehicleCount++;
+        }
+    }
+}
